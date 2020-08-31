0b8351474269826440abf8a291ebd633da42be7f
Leif Denby <leif@denby.eu>
2020-08-31 12:16:27 +0100

    add cartopy to requirements

--- setup.py
+++ setup.py
@@ -17,6 +17,7 @@ INSTALL_REQUIRES = [
     "aiohttp",
     "cfunits",
     "intake-xarray @ git+https://github.com/leifdenby/intake-xarray#egg=intake-xarray",
+    "cartopy",
 ]
 setup(
     name="eurec4a-environment",
